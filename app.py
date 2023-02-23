cac72063e6aebeb3b408a65a278595aed2441979
Vaibhav33 <65600910+Vaibhav33@users.noreply.github.com>
2023-02-23 17:20:57 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -13,17 +13,48 @@ class Todo(db.Model):
     desc=db.Column(db.String(600),nullable=False)
     date=db.Column(db.DateTime,default=datetime.utcnow)
 
-    def __repr__(self) -> str:
-        return f"{self.sno} - {self.title}"
+    # def __repr__(self) -> str:
+    #     return f"{self.sno} - {self.title}"
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def hello_world():
-    todo=Todo(title="First todo",desc="Get up earlty")
-    db.session.add(todo)
-    db.session.commit()
+    if request.method=='POST':
+        title=request.form['title']
+        desc=request.form['desc']
+
+        todo=Todo(title=title,desc=desc)
+        db.session.add(todo)
+        db.session.commit()
     allTodo=Todo.query.all()
-    print(allTodo)
+    # print(allTodo)
     return render_template('index.html',allTodo=allTodo)
+
+@app.route('/delete/<int:sno>')
+def delete(sno):
+      todo=Todo.query.filter_by(sno=sno).first()
+      db.session.delete(todo)
+      db.session.commit()
+      return redirect("/")
+
+@app.route('/update/<int:sno>',methods=['GET','POST'])
+def update(sno):
+      if request.method=='POST':
+        title=request.form['title']
+        desc=request.form['desc']
+
+        todo=Todo.query.filter_by(sno=sno).first()
+        todo.title=title
+        todo.desc=desc
+        db.session.add(todo)
+        db.session.commit()
+        return redirect("/")
+
+      todo=Todo.query.filter_by(sno=sno).first()
+      return render_template('update.html',todo=todo)
+
+
+# @app.route('/delete')
+# def delete():
 
 if __name__=='__main__':
     app.run(debug=True)
